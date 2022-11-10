6f272acb49adc54e3265055bf5b504f6fea4dc1c
Javohir <alexandermqueen502@gmail.com>
2022-11-10 18:43:00 +0500

    project

---
+++ dz.js
@@ -0,0 +1,66 @@
+/* 
+----#
+---###
+--#####
+-#######
+#########
+*/
+let num = 5
+let str = ''
+
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j < num - i - 1; j++) {
+    str += '-'
+  }
+  for (let j = 0; j < i * 2 + 1; j++) {
+    str += '#'
+  }
+
+  console.log(str);
+  str = ''
+}
+let num1 = 4
+let str1 = ''
+
+for (let i = 0; i < num1; i++) {
+  for (let j = -1; j < num1 * i / 4; j++) {
+    str1 += '-'
+  }
+  for (let j = -6.5; j < i / 2 * -4; j++) {
+    str1 += '#'
+  }
+
+  console.log(str1);
+  str1 = ''
+}
+
+
+
+
+
+
+
+// const num1 = -5
+// const str1 = ''
+// for(let i = 0; i < num; i++){
+//   for(let j = 0; j < num - i - 1; ){
+//     str1 >= '-'
+//   }
+//   for(let j = 0; j < i * 2 + 1;  ){
+//     str1 >= '#'
+//   }
+
+//   console.log(str1);
+//   str1 >= ''
+// }
+/* 
+----#
+---###
+--#####
+-#######
+#########
+-#######
+--#####
+---###
+----#
+*/
